f1c08c2e1e884094d5dce268790a729acea02bc9
MoeinAmirian07 <moein07amiry@gamil.com>
2022-05-24 23:31:34 +0430

    login-fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { AppProvider } from "@shopify/polaris";
 import en from "@shopify/polaris/locales/en.json";
 import App from "./App";
 import "@shopify/polaris/build/esm/styles.css";
-import { createRoot } from "react-dom/client";
+import createRoot from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
